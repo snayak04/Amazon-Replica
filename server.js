9b9cbd3cbeb1a6e65a3e6807060cf0ce3a8b4e87
snayak04 <snayak04.96@gmail.com>
2017-10-12 23:52:38 -0400

    Database authentication testing

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ var morgan = require('morgan');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var app = express(); //Shorthand to access express API.
-
-var User = require('/models/user'); //User object
+var ejs = require('ejs');
+var ejsMate = require('ejs-mate');
+var User = require('./models/user'); //User object
 
 mongoose.connect('mongodb://root:admin@ds117625.mlab.com:17625/amazon-replica', function(err){
     if(err){
@@ -15,26 +16,32 @@ mongoose.connect('mongodb://root:admin@ds117625.mlab.com:17625/amazon-replica', 
 })
 //Middleware - a way of invoking express into Morgan library.
 app.use(morgan('dev')); 
-app.use(bodyParser.json());
-app.user(bodyParser.urlencoded({extedted: true}));
+app.use(bodyParser.json()); //Express can parse json data
+app.use(bodyParser.urlencoded({extented: true})); //Express can parse the URL
+app.engine('ejs', ejsMate); //Tells what king of engine we are using.
+app.set('view engine', 'ejs'); //Sets the enginge to whatever we want.
 
-app.post('/signup', function(req, res){
+
+//Middleware ENDS...
+
+app.post('/signup', function(req, res, next){
     var user = new User();
-    
+     
     user.profile.name = req.body.name;
     user.password = req.body.password;
+    user.email = req.body.email;
+    
+    user.save(function(err){
+        if(err) return next(err);
+        res.json("Success!");
+    });
 });
-
-app.get('/testing', function(req, res){
-    var name = "SUperr!";
-    res.json("My Name is " + name);
-}); 
 
 
 app.get('/', function(req, res){
-    var name = "Swarup";
-    res.json("My Name is " + name);
-}); //Server get this from URL or routes "/". res is server responding, and req is server requesting.
+    res.render('home');
+})
+//Server get this from URL or routes "/". res is server responding, and req is server requesting.
 
 //app.post()//Posting data to the server
 
